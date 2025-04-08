ff55c554ec4d7bbfeb9955853a1332a768fc4f84
QuimCode <quimafoncode@gmail.com>
2025-04-08 20:33:07 -0300

    V0.1

--- Clase01/hello_world.py
+++ Clase01/hello_world.py
@@ -1,3 +1,16 @@
+# Tipos de Datos 
+
+numberVariable = 10 # Variable de tipo entero (int)
+floatVariable = 10.5 # Variable de tipo flotante (float)
+stringVariable = "Hola Mundo" # Variable de tipo cadena (str)
+booleanVariable = True # Variable de tipo booleano (bool)
+
+# Variables de tipo colección
+# Estas variables son colecciones de datos, como listas, tuplas, diccionarios y conjuntos.
+listVariable = [1, 2, 3, 4] # Variable de tipo lista (list), una lista es una colección ordenada y mutable de elementos.
+tupleVariable = (1, 2, 3, 4) # Variable de tipo tupla (tuple), una tupla es una colección ordenada e inmutable de elementos.
+dictVariable = {"key": "value"} # Variable de tipo diccionario (dict), un diccionario es una colección no ordenada de pares clave-valor.
+setVariable = {1, 2, 3, 4} # Variable de tipo conjunto (set), un conjunto es una colección no ordenada de elementos únicos.
 
 
 # Un print utilizado para verificar el funcionamiento del programa, en este caso se utiliza para mostrar un mensaje de bienvenida al usuario.
