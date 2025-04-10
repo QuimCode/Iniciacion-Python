f876776a911a3698a5b2f1dc917626b1fca6b9cf
QuimCode <quimafoncode@gmail.com>
2025-04-09 21:59:40 -0300

    V0.1

--- Clase03/functions.py
+++ Clase03/functions.py
@@ -1,3 +1,7 @@
+''' Las funciones son bloques de codigo que se pueden reutilizar, por lo que no es necesario volver a escribir el mismo bloque de codigo 
+para realizar la misma tarea. Tambien funciones se definen con la palabra reservada def, seguido del nombre de la funcion y los argumentos 
+que recibe entre parentesis. Por ende las funciones pueden recibir argumentos y devolver valores, lo que permite 
+una mayor flexibilidad y reutilizacion del codigo. '''
 
 texto= "Hola Mundo"
 def mostrar_mensaje(mensaje):
